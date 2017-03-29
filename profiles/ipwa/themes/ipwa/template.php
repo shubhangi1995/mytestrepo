ab05387f852156178ab08b019d273b07894f8b1a
sjohri <Shubhangi.Johri@init.de>
2017-03-29 17:26:27 +0530

    hidding the title in ct bild

--- profiles/ipwa/themes/ipwa/template.php
+++ profiles/ipwa/themes/ipwa/template.php
@@ -3,3 +3,27 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+/**
+ * Implements theme_preprocess_node().
+ *
+ * Theme preprocess function for node.tpl.php
+ *
+ * @params $variables
+ *
+ */
+function ipwa_preprocess_node(&$variables)
+{
+  if ($variables['type'] == 'bild') {
+    // Don't show title
+    $variables['title'] = '';
+
+    if (!empty($variables['content']['field_copyright'])) {
+      $variables['content']['field_copyright']['#title'] = '© Bildquelle';
+    }
+
+}
+  if ($variables['type'] =='article'|| $variables['type'] =='publikation'){
+    $variables['content']['field_Quellenangabe']['#title'] = 'Quelle: ';
+  }
+}
