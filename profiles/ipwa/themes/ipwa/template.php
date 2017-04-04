a739e13b7361d6da03d6c7f19a741beb5b852e2a
sjohri <Shubhangi.Johri@init.de>
2017-04-04 18:10:30 +0530

    initial  feature commit

--- profiles/ipwa/themes/ipwa/template.php
+++ profiles/ipwa/themes/ipwa/template.php
@@ -12,20 +12,62 @@
  * @params $variables
  *
  */
-function ipwa_preprocess_node(&$variables)
-{
+function ipwa_preprocess_node(&$variables) {
   if ($variables['type'] == 'bild') {
     // Don't show title
     $variables['title'] = '';
-
-    // Label change for field 'Copyright'
-    if (!empty($variables['content']['field_copyright'])) {
-      $variables['content']['field_copyright']['#title'] = '© Bildquelle';
-    }
   }
-  // Label change for field 'Videoquelle'
-  if (!empty($variables['content']['field_videoquelle'])) {
-    $variables['content']['field_videoquelle']['#title'] = '© Videoquelle';
+
+  // Display view 'Kommende Veranstaltungen' instaed of field 'Veranstaltungen'
+  $views = views_get_view_result("related_content", "block_1", $variables['nid']);
+  $related_event_view = '';
+  if (!empty($views)) {
+    $related_event_view = '<h2 class="view-title">' . t('Kommende Veranstaltungen') . '</h2>';
+    //for embedding view Kommende Veranstaltungen
+    $related_event_view .= views_embed_view("related_content", "block_1", $variables['nid']);
+    $variables['content']['related_event']['#markup'] = $related_event_view;
+    $variables['content']['related_event']['#weight'] = $variables['elements']['#fieldgroups']['group_related_event']->weight;
+  }
+
+  // display view 'Dokumente und Publikationen' instead of field 'Dokumente und Publikationen'
+  $view = views_get_view_result("related_content", "block_2", $variables['nid']);
+  $related_doc_view = '';
+  if (!empty($view)) {
+    $related_doc_view = '<h2 class="view-title">' . t('Dokumente und Publikationen') . '</h2>';
+    //for embedding view Kommende Veranstaltungen
+    $related_doc_view .= views_embed_view("related_content", "block_2", $variables['nid']);
+    $variables['content']['related_doc']['#markup'] = $related_doc_view;
+    $variables['content']['related_doc']['#weight'] = $variables['elements']['#fieldgroups']['group_related_doc']->weight;
+  }
+
+  // Change position of title field
+  // (mostly will be displayed under date field)
+  if(isset($variables['title']) && $variables['title']){
+    $variables['content']['title'] = array(
+      '#markup' => '<h1 class="page-title">' . $variables['title'] . '</h1>',
+      '#weight' => $variables['elements']['#fieldgroups']['group_title']->weight,);
+  }
+}
+
+
+/**
+ * Implements ipwa_preprocess_field().
+ *
+ * Theme preprocess function for field.tpl.php
+ *
+ * @params $variables
+ *
+ */
+
+// to link the lable of referenced node with the actual node
+function ipwa_preprocess_field(&$variables)
+{
+ $fields = array("field_meldungen_zum_thema","field_initiatoren_zum_thema", "field_projekte_zum_thema", "field_f_rderbekanntmachungen", "field_publikation");
+  if (in_array($variables['element']['#field_name'],$fields)) {
+    foreach ($variables['items'] as $key => $item) {
+      // alter field value to link to respective node.
+      $variables['items'][$key]['#markup'] = l($item['#markup'], 'node/' . $variables['element']['#items'][$key]['target_id']);
+    }
   }
 }
 
