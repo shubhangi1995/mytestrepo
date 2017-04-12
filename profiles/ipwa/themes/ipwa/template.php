c4b69743e49a15188cb985873e0ab49c4ed70fcf
sjohri <Shubhangi.Johri@init.de>
2017-04-12 18:38:27 +0530

    commit for adding fields to ct protagonists issue ipwa4-7

--- profiles/ipwa/themes/ipwa/template.php
+++ profiles/ipwa/themes/ipwa/template.php
@@ -12,14 +12,11 @@
  * @params $variables
  *
  */
-function ipwa_preprocess_node(&$variables)
-{
+function ipwa_preprocess_node(&$variables) {
   if ($variables['type'] == 'bild') {
     // Don't show title
     $variables['title'] = '';
   }
-//  print_r($variables['content'] );
-//  die();
   // Display view 'Kommende Veranstaltungen' instaed of field 'Veranstaltungen'
   if (!empty($variables['field_veranstaltungen'])) {
     $views = views_get_view_result("related_content", "block_1", $variables['nid']);
@@ -33,26 +30,26 @@ function ipwa_preprocess_node(&$variables)
   }
   }
 
-  // display view 'Dokumente und Publikationen' instead of field 'Dokumente und Publikationen'
+  // display view 'Dokumente und Publikation' instead of field 'Dokumente und Publikationen'
   if (!empty($variables['field_dokumente_und_publikatione'])) {
     $view = views_get_view_result("related_content", "block_2", $variables['nid']);
     $related_doc_view = '';
     if (!empty($view)) {
       $related_doc_view = '<h2 class="view-title">' . t('Dokumente und Publikationen') . '</h2>';
-      //for embedding view Kommende Veranstaltungen
+      //for embedding view Dokumente und Publikation
       $related_doc_view .= views_embed_view("related_content", "block_2", $variables['nid']);
       $variables['content']['related_doc']['#markup'] = $related_doc_view;
       $variables['content']['related_doc']['#weight'] = isset($variables['elements']['#fieldgroups']['group_related_doc']) ? $variables['elements']['#fieldgroups']['group_related_doc']->weight : -1;
     }
   }
 
-
+// display view 'Publikationen' instead of field 'Publikation'
   if (!empty($variables['field_publication'])) {
     $view = views_get_view_result("related_content", "block_3", $variables['nid']);
     $related_pub_view = '';
     if (!empty($view)) {
       $related_pub_view = '<h2 class="view-title">' . t('Publikation') . '</h2>';
-      //for embedding view Kommende Veranstaltungen
+      //for embedding view publikation
       $related_pub_view .= views_embed_view("related_content", "block_3", $variables['nid']);
       $variables['content']['rel_pub']['#markup'] = $related_pub_view;
       $variables['content']['rel_pub']['#weight'] = isset($variables['elements']['#fieldgroups']['group_title']) ? $variables['elements']['#fieldgroups']['group_title']->weight : -1;
@@ -67,10 +64,6 @@ function ipwa_preprocess_node(&$variables)
         '#weight' => isset($variables['elements']['#fieldgroups']['group_title']) ? $variables['elements']['#fieldgroups']['group_title']->weight : -1);
     }
   }
-  /*else {
-    print_r( '<h1 class="page-title">' . $variables['title'] . '</h1>');
-  }*/
-
 
 
   /**
@@ -83,8 +76,7 @@ function ipwa_preprocess_node(&$variables)
    */
 
 // to link the lable of referenced node with the actual node
-  function ipwa_preprocess_field(&$variables)
-  {
+  function ipwa_preprocess_field(&$variables) {
     $fields = array("field_meldungen_zum_thema", "field_initiatoren_zum_thema", "field_projekte_zum_thema", "field_f_rderbekanntmachungen", "field_publikation");
     if (in_array($variables['element']['#field_name'], $fields)) {
       foreach ($variables['items'] as $key => $item) {
