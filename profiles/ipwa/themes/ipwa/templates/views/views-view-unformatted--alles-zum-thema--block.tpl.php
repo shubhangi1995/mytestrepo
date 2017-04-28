49b45329878159c84d0cec4883c15aeb50f357b9
sjohri <Shubhangi.Johri@init.de>
2017-04-28 15:46:11 +0530

    IPWA4-42 Commit for changing the fields of CT Dokument

---
+++ profiles/ipwa/themes/ipwa/templates/views/views-view-unformatted--alles-zum-thema--block.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pram
+ * Date: 4/28/2017
+ * Time: 1:46 PM
+ */
+
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <div class="row-container">
+      <?php print $row; ?>
+      <?php print $view;?>
+    </div>
+  </div>
+<?php endforeach; ?>
