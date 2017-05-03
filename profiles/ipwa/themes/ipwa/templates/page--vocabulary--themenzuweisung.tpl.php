b50eea6782132a4776e76e01f66520b58b87e772
INIT\cchoudhary <chitra.choudhary@init.co.in>
2017-05-03 16:37:26 +0530

    IPWA-39 removed extra title on term pages

--- profiles/ipwa/themes/ipwa/templates/page--vocabulary--themenzuweisung.tpl.php
+++ profiles/ipwa/themes/ipwa/templates/page--vocabulary--themenzuweisung.tpl.php
@@ -138,11 +138,7 @@
       <?php endif; ?>
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title) && empty($node)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
+
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
