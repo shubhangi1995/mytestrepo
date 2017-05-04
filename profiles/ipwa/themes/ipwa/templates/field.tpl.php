12c6d628be75740dea087c9eaf7cdf0e82f35425
sjohri <Shubhangi.Johri@init.de>
2017-05-04 14:43:29 +0530

    IPWA4-7 Commit for changing the main display of ct Protagoninst

--- profiles/ipwa/themes/ipwa/templates/field.tpl.php
+++ profiles/ipwa/themes/ipwa/templates/field.tpl.php
@@ -44,15 +44,14 @@
  * @ingroup themeable
  */
 ?>
-<!--
-THIS FILE IS NOT USED AND IS HERE AS A STARTING POINT FOR CUSTOMIZATION ONLY.
-See http://api.drupal.org/api/function/theme_field/7 for details.
-After copying this file to your theme's folder and customizing it, remove this
-HTML comment.
--->
+
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if (!$label_hidden): ?>
-    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>&nbsp;</div>
+  <?php if ((!$label_hidden)): ?>
+    <?php if ($element['#field_name'] == 'field_copyright' || $element['#field_name'] == 'field_videoquelle'):?>
+    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
+    <?php else: ?>
+      <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>&nbsp;</div>
+    <?php endif; ?>
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
