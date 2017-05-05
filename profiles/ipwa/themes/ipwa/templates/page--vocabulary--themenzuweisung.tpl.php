88fc6f56b1511504d08105cd5e6b06d227268ae9
sjohri <Shubhangi.Johri@init.de>
2017-05-05 17:38:09 +0530

    IPWA4-16 commit for change in view "Alles Zum Thema"

--- profiles/ipwa/themes/ipwa/templates/page--vocabulary--themenzuweisung.tpl.php
+++ profiles/ipwa/themes/ipwa/templates/page--vocabulary--themenzuweisung.tpl.php
@@ -210,7 +210,7 @@
         <?php  $view = views_get_view_result("alles_zum_thema", "block",arg(2)); ?>
         <?php $term_view = ''; ?>
         <?php if (!empty($view)): ?>
-          <?php $term_view = '<h2 class="view-title">' . t('Alles zum thema') . '</h2>'; ?>
+          <?php $term_view = '<h2 class="view-title">' . t('Alles Zum Thema') . '</h2>'; ?>
           <?php //for embedding view 'Alles zum thema'
           $term_view .= views_embed_view("alles_zum_thema", "block", arg(2)); ?>
           <?php print $term_view; ?>
