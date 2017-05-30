f040a37216b1ec7c9ad710f5bbc29cbb0beb7571
sjohri <Shubhangi.Johri@init.de>
2017-05-30 16:23:36 +0530

    commit for removing errorlog related to field komende verstalengen

--- profiles/ipwa/themes/ipwa/templates/field--field_publikation.tpl.php
+++ profiles/ipwa/themes/ipwa/templates/field--field_publikation.tpl.php
@@ -55,10 +55,10 @@ HTML comment.
    <div class="field-label view-title"><h3><?php print $title_attributes; ?><?php print $label ?>&nbsp</h3></div>
   <?php endif; ?>
 
-  <?php if(!empty("field_publikation")):?>
+  <?php if(!empty("field_publikation") && isset($items)):?>
     <?php  $view = views_get_view_result("related_content", "block_3", $variables["nid"]);?>
     <?php $related_publikation = '';?>
-    <?php if (!empty($view)) :?>
+    <?php if (!empty($view) && isset($view)) :?>
       <?php $related_publikation .= views_embed_view("related_content", "block_3", $variables["nid"]);?>
       <?php print_r($related_publikation);?>
     <?php endif;?>
