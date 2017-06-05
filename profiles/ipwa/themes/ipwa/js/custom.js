367e74aeead1080ff4ae3f3a11d272907f30dc28
INIT\skaur <Satinder.kaur@init.co.in>
2017-06-05 15:37:11 +0530

    IPWA4-60

--- profiles/ipwa/themes/ipwa/js/custom.js
+++ profiles/ipwa/themes/ipwa/js/custom.js
@@ -1,3 +1,6 @@
+
+
+
 jQuery(document).ready(function(){
 
     //Detection for external links
@@ -194,5 +197,9 @@ jQuery('.tb-megamenu-submenu.dropdown-menu').attr('style','display:none !importa
 	jQuery(".date-views-pager, .views-widget-sort-by").wrapAll("<div>");
 	
 
+	 jQuery(".page-taxonomy-term .view-alles-zum-thema .view-sub-wrapper .view-filters .views-exposed-form .views-exposed-widgets .views-widget-sort-by .bef-select-as-links .form-type-bef-link:first-child a").addClass("active");
+	
 	
 });
+
+
