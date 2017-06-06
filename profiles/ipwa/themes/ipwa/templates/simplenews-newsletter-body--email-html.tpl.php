2ea0b60f805eb780cfc5b94b8e3beb2198cfc6d9
Your Name <you@example.com>
2017-06-06 16:14:04 +0530

    Worked on the news letter

--- profiles/ipwa/themes/ipwa/templates/simplenews-newsletter-body--email-html.tpl.php
+++ profiles/ipwa/themes/ipwa/templates/simplenews-newsletter-body--email-html.tpl.php
@@ -40,10 +40,15 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title><?=$title;?></title>
+	<style>
+		body{
+			display:block;
+		}
+	</style>
 </head>
 
 <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
-<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="550" id="bodyTable" style="font-family:Arial,Helvetica, sans-serif;margin:20px; font-size:16px;color:#000;">
+<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="750" id="bodyTable" style="font-family:Arial,Helvetica, sans-serif;margin:0 auto; font-size:16px;color:#000;">
     <tbody>
     <tr>
         <td>
