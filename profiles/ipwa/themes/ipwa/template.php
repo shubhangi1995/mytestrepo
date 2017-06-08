7d2bf46ed4aae95227a0035acce5464a3f0e3b65
shubhangi_1995 <shubhangi_1995@3520795.no-reply.drupal.org>
2017-06-08 12:42:07 +0530

    IPWA4-50 commit for removal of extra _

--- profiles/ipwa/themes/ipwa/template.php
+++ profiles/ipwa/themes/ipwa/template.php
@@ -55,7 +55,7 @@ function ipwa_preprocess_node(&$variables)
     // replace of 'to' with'-' for field 'datum' in CT terminprint_r
     if(!empty($variables['content']['group_page_info']['group_top_wrapper']['group_left_top_wrapper']['group_middle_wrapper']['field_event_datum'])){
       if (strpos($variables['content']['group_page_info']['group_top_wrapper']['group_left_top_wrapper']['group_middle_wrapper']['field_event_datum'][0]['#markup'], 'to') !== false) {
-        $variables['content']['group_page_info']['group_top_wrapper']['group_left_top_wrapper']['group_middle_wrapper']['field_event_datum'][0]['#markup'] = str_replace('to','_-',$variables['content']['group_page_info']['group_top_wrapper']['group_left_top_wrapper']['group_middle_wrapper']['field_event_datum'][0]['#markup']);
+        $variables['content']['group_page_info']['group_top_wrapper']['group_left_top_wrapper']['group_middle_wrapper']['field_event_datum'][0]['#markup'] = str_replace('to','-',$variables['content']['group_page_info']['group_top_wrapper']['group_left_top_wrapper']['group_middle_wrapper']['field_event_datum'][0]['#markup']);
       }
     }
   }
