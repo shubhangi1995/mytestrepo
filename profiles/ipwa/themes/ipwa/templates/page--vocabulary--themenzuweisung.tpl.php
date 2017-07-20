d0e823eb4be69e25172e5b996c8d2023aaf4f97e
shubhangi_1995 <shubhangi_1995@3520795.no-reply.drupal.org>
2017-07-20 13:12:03 +0530

    commit for change in IPWA4-140

--- profiles/ipwa/themes/ipwa/templates/page--vocabulary--themenzuweisung.tpl.php
+++ profiles/ipwa/themes/ipwa/templates/page--vocabulary--themenzuweisung.tpl.php
@@ -240,10 +240,6 @@
     <div class="footer_blocks">
       <?php $block1 = block_load("views","rss-block_1");?>
       <?php  print render(_block_get_renderable_array( _block_render_blocks(array($block1))));?>
-      <?php $block1 = block_load("ipwa_base","footer_print");?>
-      <?php  print render(_block_get_renderable_array( _block_render_blocks(array($block1))));?>
-      <?php $block = block_load("addthis","addthis_block");?>
-      <?php  print render(_block_get_renderable_array( _block_render_blocks(array($block))));?>
     </div>
   </footer>
 <?php endif; ?>
