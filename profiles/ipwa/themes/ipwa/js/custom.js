aa72edfffe95ab2158f8b1e93ddd35ce2124ce1e
Kumar <ckumar@init.de>
2017-08-28 18:15:29 +0530

    Worked on the tabindexing for back to top button

--- profiles/ipwa/themes/ipwa/js/custom.js
+++ profiles/ipwa/themes/ipwa/js/custom.js
@@ -268,7 +268,7 @@ jQuery(document).ready(function(){
 	
 	// Label tabs
 	
-	jQuery(".form-type-radio label, .search-icon").attr("tabindex", "0");
+	jQuery(".form-type-radio label, .search-icon, #backtotop").attr("tabindex", "0");
 	
 	// On enter key press
 	jQuery(".form-type-radio label, .search-icon").keypress(function (e) {
@@ -279,6 +279,15 @@ jQuery(document).ready(function(){
 			// Search input
 			jQuery("#edit-search-text").focus();
 			return false;  
+		  }
+	});
+	
+	// On enter key press upon back to top
+	jQuery("#backtotop").keypress(function (e) {
+		 var key = e.which;
+		 if(key == 13)  // the enter key code
+		  {
+			jQuery("html, body").animate({scrollTop: 0}, 600); 
 		  }
 	});
 	
