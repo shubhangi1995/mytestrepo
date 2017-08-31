ecf251b878ee4deffea83d46adc802ac87a6778a
Kumar <ckumar@init.de>
2017-08-31 15:12:12 +0530

    worked on the ical button

--- profiles/ipwa/themes/ipwa/js/custom.js
+++ profiles/ipwa/themes/ipwa/js/custom.js
@@ -495,8 +495,7 @@ jQuery(document).ready(function(){
 	//ical for iphone and ipad
 
 	jQuery(window).load(function(){
-		setTimeout(function(){
-			jQuery(window).click(function(event){
+			jQuery(window).on("click touchstart", function(event){
 					//alert("Hie");
 					jQuery(".atcb-list").hide();
 			});
@@ -508,7 +507,6 @@ jQuery(document).ready(function(){
 					// alert("not hie");
 					jQuery(".atcb-list").toggle();
 			});
-		}, 2000);
 	});
 		
 	
